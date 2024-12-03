0867a2ddf9dc07ee0221c74a464912c83abfc5de
Praveen S <praveens321123@gmail.com>
2024-12-03 10:30:43 +0530

    Regex Added!

--- forms/script.js
+++ forms/script.js
@@ -24,6 +24,12 @@ function nameHandler(){
     nameVal.style.border = "2px solid red"
     return false
   }
+  if(!name.match(/^[A-Za-z]{3,}\s{1}[A-Za-z]+$/)){
+    btn.disabled = true
+    nameErr.textContent = "FullName is Required"
+    nameVal.style.border = "2px solid red"
+    return false
+  }
   
   btn.disabled = false
   nameErr.innerHTML = `<ion-icon name="checkmark-circle"></ion-icon>`
@@ -35,11 +41,41 @@ function nameHandler(){
 nameVal.addEventListener("keyup", nameHandler)
 nameVal.addEventListener("blur", nameHandler)
 
+
+function emailHandler(){
+  const email = emailVal.value
+  // console.log(name);
+
+  if(email.length === 0){
+    btn.disabled = true
+    emailErr.textContent = "Email Required"
+    emailVal.style.border = "2px solid red"
+    return false
+  }
+  if(!email.match(/^[a-z\._$0-9]+\@[a-z]{3,}\.[a-z]{2,4}$/)){
+    btn.disabled = true
+    emailErr.textContent = "Invaild Required"
+    emailVal.style.border = "2px solid red"
+    return false
+  }
+
+  
+  btn.disabled = false
+  emailErr.innerHTML = `<ion-icon name="checkmark-circle"></ion-icon>`
+  emailVal.style.border = "2px solid green"
+  emailVal.style.outline = "none"
+  return true
+}
+
+emailVal.addEventListener("keyup", emailHandler)
+emailVal.addEventListener("blur", emailHandler)
+
+
 const form = document.querySelector("form")
 
 form.addEventListener("submit", (event)=>{
    event.preventDefault()
-   if(!nameHandler()){
+   if(!nameHandler() && !emailHandler()){
     return false
    }
 })
